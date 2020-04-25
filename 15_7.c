9dccdcb4a2ade1480dde9f7dff0e29f19817c92a
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-04-25 20:11:17 +0800

    Create 15_7.c

---
+++ 15_7.c
@@ -0,0 +1,12 @@
+/*15_7.做到我說停-圓(10.0)*/
+#include <stdio.h>
+int main(){
+    float p=1,pi=3.14;
+    while(p>0){
+        scanf("%f",&p);
+        float r=((p/(2*pi))*100)/100;
+        if(p>0)
+            printf("半徑%.2f\n面積%.2f\n",r,r*r*pi);
+    }
+	return 0;
+}
