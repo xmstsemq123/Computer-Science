aba245e5bbd2f8a3be47cfe0cbad13b2caad8c5a
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-04-25 19:14:16 +0800

    Update 11_9(Not completed.).c

--- 11_9(Not completed.).c
+++ 11_9(Not completed.).c
@@ -1,1 +1,19 @@
-
+/*11_9.多選一-3N+1-2(10.0)*/
+#include <stdio.h>
+int main(){
+    int n=0,i,j;
+    scanf("%d %d",&i,&j);
+    for(int a=j;a<=i;a++){
+        j=a;
+        while(j!=1){
+            n=n+1;
+            printf("%d ",j);
+            if(j%2==0)
+                j=j/2;
+            else
+                j=3*j+1;
+        }
+        printf("1\n迴圈次數%d\n",n);
+    }
+	return 0;
+}
