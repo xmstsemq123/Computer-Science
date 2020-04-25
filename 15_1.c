4191da2c0e7d998c3be060164b57d4c638751bc2
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-04-25 19:27:55 +0800

    Create 15_1.c

---
+++ 15_1.c
@@ -0,0 +1,11 @@
+/*15_1.做到我說停-笑到我說停(10.0)*/
+#include <stdio.h>
+int main(){
+    int a=0;
+    while(a!=-1){
+        scanf("%d",&a);
+        if(a!=-1)
+            printf("HaHaHa!\n");
+    }
+	return 0;
+}
