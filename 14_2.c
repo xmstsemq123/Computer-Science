bf775db63a86291bc9cac0ad846f87492a6bf2d5
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-04-25 18:25:51 +0800

    Create 14_2.c

---
+++ 14_2.c
@@ -0,0 +1,11 @@
+/*14_2.我說幾遍就幾遍-HaHaHa!笑他幾次(10.0)*/
+#include <stdio.h>
+int main(){
+    int i,j=0;
+    scanf("%d",&i);
+    while(j<=i){
+        printf("HaHaHa!\n");
+        j=j+1;
+    }
+	return 0;
+}
