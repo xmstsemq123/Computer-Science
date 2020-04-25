c63f3d80adf1b9141b2b6ad9044456de8ed92f6a
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-04-25 19:34:03 +0800

    Create 15_2.c

---
+++ 15_2.c
@@ -0,0 +1,11 @@
+/*15_2.做到我說停-直到我說停(10.0)*/
+#include <stdio.h>
+int main(){
+    int a;
+    while(a!=-1){
+        scanf("%d",&a);
+        if(a!=-1)
+            printf("%d\n",a);
+    }
+	return 0;
+}
