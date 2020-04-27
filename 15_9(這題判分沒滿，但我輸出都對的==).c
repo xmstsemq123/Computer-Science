973341eb3673110a145aef19a12ca4ae3cea4904
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-04-27 21:12:54 +0800

    Update 15_9(這題判分沒滿，但我輸出都對的==).c

--- 15_9(這題判分沒滿，但我輸出都對的==).c
+++ 15_9(這題判分沒滿，但我輸出都對的==).c
@@ -2,26 +2,23 @@
 #include <stdio.h>
 int main(){
     float x,y;
-    while(x+y){
-        scanf("%f %f",&x,&y);
-        if(x+y){
-            if(x!=0 and y!=0){
-                if(x>0 and y>0)
-                    printf("第一象限\n");
-                else if(x<0 and y>0)
-                    printf("第二象限\n");
-                else if(x<0 and y<0)
-                    printf("第三象限\n");
-                else if(x>0 and y<0)
-                    printf("第四象限\n");
-            }else{
-                if(y==0)
-                    printf("x軸\n");
-                else
-                    printf("y軸\n");
-            }   
-            
+    scanf("%f %f",&x,&y);
+    while(x!=0 or y!=0){
+        if(x!=0 and y!=0){
+            if(x>0 and y>0)
+                printf("第一象限\n");
+            else if(x<0 and y>0)
+                printf("第二象限\n");
+            else if(x<0 and y<0)
+                printf("第三象限\n");
+            else if(x>0 and y<0)
+                printf("第三象限\n");
         }
+        if(x==0 and y!=0)
+            printf("y軸\n");
+        else if(x!=0 and y==0)
+            printf("x軸\n");
+        scanf("%f %f",&x,&y);
     }
 	return 0;
 }
