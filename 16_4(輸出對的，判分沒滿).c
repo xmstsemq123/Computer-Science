ec65ab86f0753835068929ce61791eb79f116211
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-04-28 21:19:35 +0800

    Update 16_4(輸出對的，判分沒滿).c

--- 16_4(輸出對的，判分沒滿).c
+++ 16_4(輸出對的，判分沒滿).c
@@ -1,18 +1,16 @@
 /*16_4.if是while的好幫手-是3又是5的倍數(10.0)*/
 #include <stdio.h>
 int main(){
-    int a,b,max,min;
+    int a,b;
     scanf("%d %d",&a,&b);
     while(a!=0 and b!=0){
-        if(b>a){
-            max=b;
-            min=a;
+        if(a>b){
+            if(a%b==0)
+                printf("%d是%d的倍數\n",a,b);
         }else{
-            max=a;
-            min=b;
+            if(b%a==0)
+                printf("%d是%d的倍數\n",b,a);
         }
-        if(max%min==0)
-            printf("%d是%d的倍數\n",max,min);
         scanf("%d %d",&a,&b);
     }
 	return 0;
