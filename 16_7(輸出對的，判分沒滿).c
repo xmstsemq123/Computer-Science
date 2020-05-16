8338ae40b49cfcb041e2d8d14b97e23d5a82df08
Janlx Lu <31385048+xmstsemq123@users.noreply.github.com>
2020-05-16 08:36:00 +0800

    Update 16_7(輸出對的，判分沒滿).c

--- 16_7(輸出對的，判分沒滿).c
+++ 16_7(輸出對的，判分沒滿).c
@@ -1,17 +1,14 @@
 /*16_7.if是while的好幫手-最大公因數(10.0)*/
 #include <stdio.h>
 int main(){
-    int a,b,i=1,min;
+    int a,b,i=1;
     scanf("%d %d",&a,&b);
     printf("%d與%d的公因數",a,b);
-    min=a;
-    if(b<min)
-        min=b;
-    while(i<=min){
+    while(i<=a){
         if(a%i==b%i)
             printf("%d ",i);
-        if(min==i)
-            printf("\n%d與%d的最大公因數%d",a,b,min);
+        if(i==a)
+            printf("\n%d與%d的最大公因數%d",a,b,i);
         i=i+1;
     }
 	return 0;
